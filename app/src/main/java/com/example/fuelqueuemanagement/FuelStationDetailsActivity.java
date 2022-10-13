6d381deb675ec12803cf2e3703dc6c63f5326a7e
duhithu <duhithus@gmail.com>
2022-10-13 23:34:19 +0530

    Initial Commit - Created Fuel Station Details Interfaces

---
+++ app/src/main/java/com/example/fuelqueuemanagement/FuelStationDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.fuelqueuemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FuelStationDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fuel_station_details);
+    }
+}
