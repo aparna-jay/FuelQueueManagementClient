38e00c185651c54cf81aeb4b3d34f7e4b0bbe12f
IT19123882 <it19123882@my.sliit.lk>
2022-10-13 15:03:24 +0530

    Design Filling Station Profile

---
+++ app/src/main/java/com/example/fuelqueuemanagement/FillingStationProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.fuelqueuemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FillingStationProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filling_station_profile);
+    }
+}
