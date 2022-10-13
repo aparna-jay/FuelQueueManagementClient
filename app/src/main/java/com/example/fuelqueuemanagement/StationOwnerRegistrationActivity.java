228bda489398bd0f35261a9a9d3bbec72e7b7386
duhithu <duhithus@gmail.com>
2022-10-13 22:58:41 +0530

    Initial Commit - Created customer and station owners Interfaces

---
+++ app/src/main/java/com/example/fuelqueuemanagement/StationOwnerRegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.fuelqueuemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StationOwnerRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_station_owner_registration);
+    }
+}
